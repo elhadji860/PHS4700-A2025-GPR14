db1898cd39289b76978b696ca2754b3839cae5f2
Steven Ho <steven.ho01@outlook.com>
2025-09-27 06:30:55 -0400

    refactor: create Constants.m for constants

---
+++ Constants.m
@@ -0,0 +1,15 @@
+classdef Constants
+    properties (Constant)
+        % Fuselage
+        RAYON_FUSELAGE = 1.345;
+        
+        % Ailes
+        CENTRE_X_AILE = 10.54;
+        EPAISSEUR_AILE = 0.25;
+        
+        % Moteurs
+        CENTRE_X_MOTEUR = 5;
+        LONGUEUR_MOTEUR = 3.68;
+        RAYON_MOTEUR = 0.724;
+    end
+end
--- calculAccelerationAngulaire.m
+++ calculAccelerationAngulaire.m
@@ -7,10 +7,15 @@ end
 function MF = calculMomentForce(posA, ar, Forces, pcm)
     R_y = [cos(ar), 0, sin(ar); 0, 1, 0; -sin(ar), 0, cos(ar)];
     
-    # TODO Créer constantes
-    pos_md_local = R_y * [5 - 3.68/2; 1.345 + 0.724; 1.345 + 0.25];
-    pos_mg_local = R_y * [5 - 3.68/2; -(1.345 + 0.724); 1.345 + 0.25];
-    pos_p_local  = R_y * [10.54; 0; 0];
+    pos_md_local = R_y * [Constants.CENTRE_X_MOTEUR - Constants.LONGUEUR_MOTEUR / 2; 
+                         Constants.RAYON_FUSELAGE + Constants.RAYON_MOTEUR; 
+                         Constants.RAYON_FUSELAGE + Constants.EPAISSEUR_AILE];
+    
+    pos_mg_local = R_y * [Constants.CENTRE_X_MOTEUR - Constants.LONGUEUR_MOTEUR / 2; 
+                         -(Constants.RAYON_FUSELAGE + Constants.RAYON_MOTEUR); 
+                         Constants.RAYON_FUSELAGE + Constants.EPAISSEUR_AILE];
+    
+    pos_p_local = R_y * [Constants.CENTRE_X_AILE; 0; 0];
 
     pos_md_global = posA + pos_md_local;
     pos_mg_global = posA + pos_mg_local;
@@ -20,7 +25,7 @@ function MF = calculMomentForce(posA, ar, Forces, pcm)
     force_mg = R_y * [Forces(2); 0; 0];
     force_p  = [0; 0; Forces(3)];
 
-    MF_md = cross(pos_md_global  - pcm, force_md);
+    MF_md = cross(pos_md_global - pcm, force_md);
     MF_mg = cross(pos_mg_global - pcm, force_mg);
     MF_p  = cross(pos_p_global - pcm, force_p);
 
